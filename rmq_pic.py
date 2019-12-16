0e8c28fbbb6c5a4d2553b8b1d3226cf8304f3750
will-campbell-492 <wtc@vt.edu>
2019-12-15 19:05:26 -0500

    Removes unnecessary callback

--- rmq_pic.py
+++ rmq_pic.py
@@ -1,12 +1,5 @@
 import pika
 
-
-def callback(ch, method, properties, body):
-    print("%r:%r" % (method.routing_key, body.decode("utf-8")))
-    dataForTori = body.decode("utf-8")
-    print(dataForTori)
-    
-
 # Rabbitmq setup
 credentials = pika.PlainCredentials('repo-pi','netapps')
 connection = pika.BlockingConnection(pika.ConnectionParameters('pumpkin-pi',
